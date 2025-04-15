85271441b6014be82029fb84e668696dc38033a8
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-04-15 16:15:00 -0500

    Create Contains_Duplicate_new.py

---
+++ Contains_Duplicate_new.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+        hash = {}
+ 
+        for i in range(len(nums)):
+            if nums[i] in hash:
+                return True
+            else:
+                hash[nums[i]] = i
+        return False
